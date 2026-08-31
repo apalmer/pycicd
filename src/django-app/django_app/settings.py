f8aedbdbae752071ada4252148421a433ce15211
Adigun Palmer <adigunpalmer@hotmail.com>
2026-08-30 21:28:37 -0400

    added custom events logging

--- src/django-app/django_app/settings.py
+++ src/django-app/django_app/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 import os
 from pathlib import Path
+
+from azure.monitor.opentelemetry import configure_azure_monitor
 from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -132,9 +134,13 @@ LOGGING = {
         'console': {
             'class': 'logging.StreamHandler',
         },
+        'azure_monitor': {
+            'class': 'opentelemetry.sdk._logs.LoggingHandler',
+            'level': 'INFO',
+        },
     },
     'root': {
-        'handlers': ['console'],
-        'level': 'INFO', # This automatically syncs root logs into Azure Monitor
+        'handlers': ['console', 'azure_monitor'],
+        'level': 'INFO',
     },
 }
--- src/django-app/home/views.py
+++ src/django-app/home/views.py
@@ -9,4 +9,23 @@ def index(request):
     logger.debug(f"Request method: {request.method}, Request path: {request.path}")
     logger.warning("This is a warning message for demonstration purposes")
     logger.error("This is an error message for demonstration purposes")
+    logger.warning("WARNING: Warning log with properties", extra={"key1": "value1"})
+    # Log a custom event with a custom name and additional attribute
+    # The 'microsoft.custom_event.name' value will be used as the name of the customEvent
+    logger.warning(
+        "Hello World!",
+        extra={
+            "microsoft.custom_event.name": "test-event-name",
+            "additional_attrs": "val1"
+        }
+    )
+
+    # You can also populate fields like client_IP with attribute `client.address`
+    logger.info(
+        "This entry will have a custom client_IP",
+        extra={
+            "microsoft.custom_event.name": "test_event",
+            "client.address": "192.168.1.1"
+        }
+    )
     return render(request, 'home/index.html')
