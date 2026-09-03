3e5eb55c3be34a37d25138cafabe9c5710c850b2
Adigun Palmer <adigunpalmer@hotmail.com>
2026-09-03 09:42:16 -0400

    adding various error logs

--- src/django-app/home/views.py
+++ src/django-app/home/views.py
@@ -6,9 +6,9 @@ logger = logging.getLogger(__name__)
 # Create your views here.
 def index(request):
     logger.info("Rendering the index page")
-    # logger.debug(f"Request method: {request.method}, Request path: {request.path}")
-    # logger.warning("This is a warning message for demonstration purposes")
-    # logger.error("This is an error message for demonstration purposes")
+    logger.debug(f"Request method: {request.method}, Request path: {request.path}")
+    logger.warning("This is a warning message for demonstration purposes")
+    logger.error("This is an error message for demonstration purposes")
     # logger.warning("WARNING: Warning log with properties", extra={"key1": "value1"})
     # logger.warning(
     #     "Hello World!",
