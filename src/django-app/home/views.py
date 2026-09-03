b62515a6cc7c97d155673086a7035767149fb426
Adigun Palmer <adigunpalmer@hotmail.com>
2026-09-03 10:06:15 -0400

    adding custom events

--- src/django-app/home/views.py
+++ src/django-app/home/views.py
@@ -9,19 +9,19 @@ def index(request):
     logger.debug(f"Request method: {request.method}, Request path: {request.path}")
     logger.warning("This is a warning message for demonstration purposes")
     logger.error("This is an error message for demonstration purposes")
-    # logger.warning("WARNING: Warning log with properties", extra={"key1": "value1"})
-    # logger.warning(
-    #     "Hello World!",
-    #     extra={
-    #         "microsoft.custom_event.name": "test-event-name",
-    #         "additional_attrs": "val1"
-    #     }
-    # )
-    # logger.info(
-    #     "This entry will have a custom client_IP",
-    #     extra={
-    #         "microsoft.custom_event.name": "test_event",
-    #         "client.address": "192.168.1.1"
-    #     }
-    # )
+    logger.warning("WARNING: Warning log with properties", extra={"key1": "value1"})
+    logger.warning(
+        "Hello World!",
+        extra={
+            "microsoft.custom_event.name": "test-event-name",
+            "additional_attrs": "val1"
+        }
+    )
+    logger.info(
+        "This entry will have a custom client_IP",
+        extra={
+            "microsoft.custom_event.name": "test_event",
+            "client.address": "192.168.1.1"
+        }
+    )
     return render(request, 'home/index.html')
