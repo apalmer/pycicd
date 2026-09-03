59fc544ce1aea89d3623611837a0e94d5b5627f1
Adigun Palmer <adigunpalmer@hotmail.com>
2026-09-03 09:14:06 -0400

    adding logger.info in /

--- src/django-app/home/views.py
+++ src/django-app/home/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
 
-# import logging
-# logger = logging.getLogger(__name__)
+import logging
+logger = logging.getLogger(__name__)
 
 # Create your views here.
 def index(request):
-    # logger.info("Rendering the index page")
+    logger.info("Rendering the index page")
     # logger.debug(f"Request method: {request.method}, Request path: {request.path}")
     # logger.warning("This is a warning message for demonstration purposes")
     # logger.error("This is an error message for demonstration purposes")
